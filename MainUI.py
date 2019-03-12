b6dd8876063e3f82c7d8c6ed27132a7a8d191a94
Abraham <Abraham_Lincoln@comcast.com>
2019-03-12 12:12:41 +0530

    v0.1

---
+++ MainUI.py
@@ -0,0 +1,172 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+import pandas as pd
+import matplotlib.pyplot as plt
+from concurrent.futures import ThreadPoolExecutor
+import time
+import UserTimeline
+import traceback
+from matplotlib import interactive
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setEnabled(True)
+        self.label.setGeometry(QtCore.QRect(20, 130, 221, 16))
+        self.label.setLineWidth(2)
+        self.label.setWordWrap(True)
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(20, 160, 191, 16))
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(20, 190, 211, 16))
+        self.label_3.setObjectName("label_3")
+        self.datetime = QtWidgets.QLineEdit(self.centralwidget)
+        self.datetime.setGeometry(QtCore.QRect(250, 126, 331, 19))
+        self.datetime.setText("")
+        self.datetime.setObjectName("datetime")
+        self.duration_set_button = QtWidgets.QSpinBox(self.centralwidget)
+        self.duration_set_button.setEnabled(True)
+        self.duration_set_button.setGeometry(QtCore.QRect(250, 190, 33, 19))
+        self.duration_set_button.setMouseTracking(False)
+        self.duration_set_button.setWrapping(True)
+        self.duration_set_button.setReadOnly(False)
+        self.duration_set_button.setAccelerated(True)
+        self.duration_set_button.setCorrectionMode(QtWidgets.QAbstractSpinBox.CorrectToPreviousValue)
+        self.duration_set_button.setKeyboardTracking(True)
+        self.duration_set_button.setMinimum(1)
+        self.duration_set_button.setMaximum(6)
+        self.duration_set_button.setSingleStep(1)
+        self.duration_set_button.setProperty("value", 1)
+        self.duration_set_button.setDisplayIntegerBase(10)
+        self.duration_set_button.setObjectName("duration_set_button")
+        self.run_button = QtWidgets.QPushButton(self.centralwidget)
+        self.run_button.setGeometry(QtCore.QRect(250, 250, 56, 17))
+        self.run_button.setObjectName("run_button")
+
+        self.filename = QtWidgets.QLineEdit(self.centralwidget)
+        self.filename.setGeometry(QtCore.QRect(250, 160, 201, 20))
+        self.filename.setObjectName("filename")
+
+
+
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 18))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        self.run_button.clicked.connect(self.whenpressedrun)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+
+        self.executor = ThreadPoolExecutor(max_workers=2)
+
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "Enter starting date and time:"))
+        self.label_2.setText(_translate("MainWindow", "Enter file name:"))
+        self.label_3.setText(_translate("MainWindow", "Duration(in hours):"))
+        self.run_button.setText(_translate("MainWindow", "Run"))
+
+    def whenpressedrun(self):
+        date_str=self.datetime.text().strip()
+        dur_str=self.duration_set_button.text()
+        file_name=self.filename.text()
+        print("run pressed!")
+        pattern = '%Y %b %d %H:%M:%S.%f'
+        epoch1 = int(time.mktime(time.strptime(date_str, pattern)))
+        int_dur = int(dur_str)
+        int_dur = int_dur * 3600
+        epoch2 = epoch1 - int_dur
+        mainlist = UserTimeline.getMarkers(epoch2,epoch1,file_name)
+        mlist = UserTimeline.getPlaybackmode(mainlist)
+        print(mlist)
+        nlist = UserTimeline.getSettings(mainlist)
+        print(nlist)
+
+        try:
+            df = pd.DataFrame(mlist)
+            df1 = pd.DataFrame(nlist)
+            df.columns = ['dates', 'col1', 'col2', 'col3']
+            df1.columns = ['dates', 'col1', 'col2', 'col3']
+            df = df.sort_values(by='dates')
+            df1 = df1.sort_values(by='dates')
+
+            df.to_csv('videoTimeline.csv', index=False)
+            df1.to_csv('settingsTimeline.csv', index=False)
+
+            df1["marker"] = df1["col1"] + df1["col2"]
+            df["marker"] = df["col1"] + df["col2"]
+            datelistdf1 = list(df1.dates)
+            datelistdf = list(df.dates)
+
+            markerlistdf1 = list(df1.marker)
+            markerlistdf = list(df.marker)
+
+            col3listdf1 = list(df1.col3)
+            col3listdf = list(df.col3)
+            #interactive(True)
+            #self.first_graph(col3listdf,datelistdf,markerlistdf)
+            #self.second_graph(col3listdf1,datelistdf1,markerlistdf1)
+            fig, ax = plt.subplots()
+            ax.scatter(col3listdf, datelistdf)
+
+            for i, txt in enumerate(markerlistdf):
+                ax.annotate(txt, (col3listdf[i], datelistdf[i]))
+            interactive(True)
+            plt.show()
+
+            fig, ax = plt.subplots()
+            ax.scatter(col3listdf1, datelistdf1)
+
+            for j, txt in enumerate(markerlistdf1):
+                ax.annotate(txt, (col3listdf1[j], datelistdf1[j]))
+            interactive(False)
+
+            plt.show()
+
+        except:
+            print("Dataframe failed")
+            traceback.print_exc()
+
+    #def first_graph(self,col3listdf,datelistdf,markerlistdf):
+        #fig, ax = plt.subplots()
+        #ax.scatter(col3listdf, datelistdf)
+
+        #for i, txt in enumerate(markerlistdf):
+            #ax.annotate(txt, (col3listdf[i], datelistdf[i]))
+        #plt.show()
+        #return True
+
+
+    #def second_graph(self,col3listdf1,datelistdf1,markerlistdf1):
+        #fig, ax = plt.subplots()
+        #ax.scatter(col3listdf1, datelistdf1)
+
+        #for j, txt in enumerate(markerlistdf1):
+            #ax.annotate(txt, (col3listdf1[j], datelistdf1[j]))
+
+        #plt.show()
+        #return True
+if __name__ == "__main__":
+    import sys
+
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+
+    sys.exit(app.exec_())
---
+++ UserTimeline.py
@@ -0,0 +1,240 @@
+import csv
+import re
+import pandas as pd
+import matplotlib.pyplot as plt
+import argparse
+from concurrent.futures import ThreadPoolExecutor
+
+import time
+from keypress import keyDict
+
+
+
+def getMarkers(epoch1,epoch2,file_name):
+        with open('strings_receiver.csv', 'r') as csv_file:
+            text = csv.reader(csv_file)
+            markerDict = {}
+            markerDict[file_name] = []
+            for line in text:
+                try:
+                    #if line[1] not in markerDict.keys():
+                    #markerDict[args.inputfile] = []
+                    markerDict[file_name].append(line[0])
+                    #print("Got markerDict!")
+                except:
+                    print("Failed to get markerDict!")
+                    pass
+        #print(markerDict)
+        mainlist=[]
+        count=0
+        for x in markerDict:
+            a=open(x ,'r')
+            b=a.readlines()
+            for word in markerDict[x]:
+                for line in b:
+                    if word in line:
+                        st = re.compile(r'\d\d\d\d\s[a-zA-Z][a-zA-Z][a-zA-Z]\s\d\d\s\d\d.\d\d.\d\d.\d\d\d\d\d\d')
+                        result = st.findall(line)
+                        #print(result)
+                        date = " ".join(result)
+                        #print(date)
+                        pattern = '%Y %b %d %H:%M:%S.%f'
+                        epoch3 = int(time.mktime(time.strptime(date, pattern)))
+                        #print(epoch3)
+                        #count=count+1
+                        if epoch3>=epoch1 and epoch3<=epoch2:
+                            #print(date)
+                            #count=count+1
+                            if word=="XREKeyPressLog":
+                                regex2 = re.compile('%s.+' % word)
+                                result2 = regex2.findall(line)
+                                split1=result2[0].split(" ")
+                                rr=split1[3]
+                                #print(rr)
+                                if(rr in keyDict):
+                                    #print(keyDict[rr])
+                                    #count=count+1
+                                    mark = " ".join(keyDict[rr])
+                                    list = []
+                                    list.append(date)
+                                    list.append(mark)
+                                    # list.append(x)
+                                    mainlist.append(list)
+                                #print(result2)
+                            else:
+                                regex2 = re.compile('%s.+' %word)
+                                result2=regex2.findall(line)
+                                #print(result2)
+                                mark = " ".join(result2)
+                                count=count+1
+                                list = []
+                                list.append(date)
+                                list.append(mark)
+                                # list.append(x)
+                                mainlist.append(list)
+                                #print(mainlist)
+                        #date = " ".join(result)
+
+        return mainlist
+
+
+
+
+
+def getPlaybackmode(mainlist):
+    #for lines in mainlist:
+    finalist = []
+    for line in mainlist:
+        if "playbackmode" in line[1]:
+            if "tune request" in line[1]:
+                #print("request url")
+                if("ocap" in line[1]):
+                    #line[1] = "Qam Linear"
+                    word = "ocap://"
+                    reg = re.compile('%s.+&res' % word)
+                    result = reg.findall(line[1])
+                    line[1] = "QAM Linear"
+                    line.append(''.join(result)[7:-4])
+                    line.append("receiverlog")
+
+                    finalist.append(line)
+
+                elif("VOD" in line[1]):
+                    word = "net/"
+                    reg = re.compile('%s.+/movie' % word)
+                    result = reg.findall(line[1])
+                    line[1] = "VOD URL"
+                    line.append(''.join(result)[3:-6])
+                    line.append("receiverlog")
+
+                    finalist.append(line)
+
+                elif ("DVR" in line[1]):
+                    word = "cdvr-"
+                    reg = re.compile('%s.+xcr' % word)
+                    result = reg.findall(line[1])
+                    line[1] = "DVR URL"
+                    line.append(''.join(result)[5:-4])
+                    line.append("receiverlog")
+
+                    finalist.append(line)
+
+                elif("m3u8" in line[1]):
+                    #line[1] = "Ip linear"
+                    if '.net%2F' in line[1]:
+                        word = ".net%2F"
+                    elif '.com%2F' in line[1]:
+                        word = ".com%2F"
+                    reg = re.compile('%s.+' % word)
+                    result = reg.findall(line[1])
+                    line[1] = "IP Linear"
+                    line.append(''.join(result)[7:20])
+                    line.append("receiverlog")
+
+                    finalist.append(line)
+
+            elif "succeeded" in line[1]:
+                line[1] = "Video Tune Succeeded"
+                line.append(" ")
+                line.append("receiverlog")
+                finalist.append(line)
+
+                #print("success url")
+            elif "failed" in line[1]:
+                line[1] = "Video Failed"
+                line.append(" ")
+                line.append("receiverlog")
+                finalist.append(line)
+
+
+        elif "K E D" in line[1]:
+            #finalist.append(line)
+            pass
+        elif "HTML5 video" in line[1]:
+            word = "mediasourceblob:"
+            reg = re.compile('%s.+' % word)
+            result = reg.findall(line[1])
+            if "Loading" in line[1]:
+                line[1] = "Loading"
+                line.append(''.join(result)[16:-1])
+                line.append("receiverlog")
+                finalist.append(line)
+
+            elif "Pause" in line[1]:
+                line[1] = "Pause"
+                line.append(''.join(result)[16:-1])
+                line.append("receiverlog")
+                finalist.append(line)
+            elif "Playback started" in line[1]:
+                line[1] = "Playback started"
+                line.append(''.join(result)[16:-1])
+                line.append("receiverlog")
+                finalist.append(line)
+
+            elif "Playback terminated" in line[1]:
+                line[1] = "Playback terminated"
+                line.append(''.join(result)[16:-1])
+                line.append("receiverlog")
+                finalist.append(line)
+                #finalist.append("receiver.log")
+            elif "Play " in line[1]:
+                line[1] = "Play"
+                line.append(''.join(result)[16:-1])
+                line.append("receiverlog")
+                finalist.append(line)
+
+            else:
+                pass
+
+        elif "Gibbon" in line[1]:
+            if "GibbonOEM_Init" in line[1]:
+                line[1] = "Netflix initiated"
+                line.append(" ")
+                line.append("receiverlog")
+                finalist.append(line)
+
+            elif "Gibbon_Start" or "Gibbon_Started" in line[1]:
+                line[1]="Netflix started"
+                line.append(" ")
+                line.append("receiverlog")
+                finalist.append(line)
+        else:
+            pass
+
+
+    return finalist
+
+def getSettings(mainlist):
+    finalist = []
+    for line in mainlist:
+        if "closedcaptions" in line[1]:
+
+            line[1] = "closed captions"
+            line.append(" ")
+
+            line.append("receiverlog")
+            finalist.append(line)
+
+        elif "SAP" in line[1]:
+            word = "SAP"
+            reg = re.compile('%s.+' % word)
+            result = reg.findall(line[1])
+            result = " ".join(result)
+
+            line[1] = result
+            line.append(" ")
+            line.append("receiverlog")
+            finalist.append(line)
+
+        else:
+            pass
+
+    return finalist
+
+
+
+
+
+
+
+
